86c682e676794387004bf805914bb4346431da13
Carolingie1992 <bonet.ortega@gmail.com>
2017-04-25 10:32:20 +0200

    blabla

--- src/com/gmv/vodafone/OfertaDAO.java
+++ src/com/gmv/vodafone/OfertaDAO.java
@@ -1,9 +1,11 @@
 package com.gmv.vodafone;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OfertaDAO {
 	List<Oferta> getOfertas() throws OfertaException;
 	
 	Oferta getOferta(int codigoOferta) throws OfertaException;
+	LocalDate getFechaOferta(int codigoOferta);
 }
